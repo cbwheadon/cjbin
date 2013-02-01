a786c04b53516b3e8eb68eae5548804435ebefca
Chris Wheadon <chris.wheadon@gmail.com>
2013-02-01 09:03:15 +0000

    infit

--- conn.scala
+++ conn.scala
@@ -12,24 +12,33 @@ object conn {
   
   def main(args: Array[String])  = {
     
-    //assessment id
+    //assessment id = sup
     val sup = args(0).toInt
     
+    //Connect to database
     Database.forURL("jdbc:mysql://nmm.cjibvszqadnr.eu-west-1.rds.amazonaws.com:3306/nmm", driver = "com.mysql.jdbc.Driver", user = "django", password="z5A!a4!c") withSession {
     
+    //Run get judgements stored procedure  
     val q = sql"call get_judgements_sp ($sup)".as[Judgement]
-       
+    
+    //Extract stored proc results into a list of Judgements
     val js = q.list()
-    var uids = extractIds(js, Nil)
-    var i4 = iter(uids,js,4)
+    //Extract unique candidate ids
+    val uids = extractIds(js, Nil)
+    //Calculate true scores
+    val i4 = iter(uids,js,4)
+    //Calculate interrater reliability
+    val ir = irrel(uids,js,100)
+    //Standard deviation of current parameter values
+    val alpha = raschRel(i4)
     
-    // Update
-    i4.foreach(println)
-    println("all")
     
-    var ir = irrel(uids,js,1000)
-    println("Reliability:" + ir)
+    //Extract unique marker ids
+    val markers = uniqueMarker(js, Nil)
+    //Calculate marker infit
+    val sumRes = markerInfit(i4, js, markers,Nil)
     
+    //Update candidate true scores
     def update(ts: TrueScore) = (Q.u + "update nmm.assess_userassessment " +
     		"set truescore = " +? ts.true_score + ", " +
     		"rawscore = " +? ts.obs + ", " +
@@ -38,24 +47,22 @@ object conn {
     		"timetaken = " +? ts.timetaken +
     		" where id = "+? ts.id +";").execute
       
-    
-    		 
-    /*
-    def update(ts: TrueScore) = (Q.u + "call nmm.update_true_scores " +
-    		"(" +? ts.true_score + 
-    		"," +? ts.obs + 
-    		"," +? ts.se + 
-    		"," +? ts.comps + 
-    		"," +? ts.timetaken +
-    		","+? ts.id +");").execute
-    		 
-    */
-    		
-    //call update_true_scores (5.5555, 101, 0.001, 102, 33.3, 107)
-    
     i4.foreach(update)
-    println(i4.length.toString + " records updated")
     
+    //Update marker infit
+    def updateMarkers(marker: Marker) = (Q.u + "update nmm.assess_marker " +
+    		"set truescore = " +? marker.infit + 
+    		" where user_id = "+? marker.id +";").execute
+    
+    sumRes.foreach(updateMarkers)		
+    
+    //Print out results
+    i4.foreach(println)
+    sumRes.foreach(println)
+    println("Reliability:" + ir)
+    println(i4.length.toString + " candidate records updated")
+    println(sumRes.length.toString + " marker records updated")
+    println("Rasch Reliability: " + alpha)
     //Needs this for questaurus
     //val assignment:Assessment = new Assessment(id=sup)
     //def update_ass(ass: Assessment) = (Q.u + "update questaurus.assess_assessment " +
--- ml.scala
+++ ml.scala
@@ -7,6 +7,7 @@ object ml {
   
   case class Assessment(id: Int)
   case class Judgement(userchosen: Int, usernotchosen: Int, timetaken: Double, marker:Int)
+  case class Marker(id:Int, infit:Double)
   case class TrueScore(id: Int, true_score: Double, se:Double, obs:Int, comps:Int, timetaken:Double)
   case class Contest(opponent_ability:Double, result:Int)
   
@@ -14,7 +15,37 @@ object ml {
      case Nil => uids.distinct
      case _ =>
        extractIds(ids.tail, new TrueScore(ids.head.userchosen,0.0,0.0,0,0,0.0) :: new TrueScore(ids.head.usernotchosen,0.0,0.0,0,0,0.0) :: uids)
-  }                                               
+  }
+  
+  def uniqueMarker(ids: List[Judgement], markers: List[Marker]): List[Marker] = ids match {
+    case Nil => markers.distinct
+    case _ =>
+      uniqueMarker(ids.tail, new Marker(ids.head.marker,0.0) :: markers)  
+  } 
+  
+  def markerInfit(ts: List[TrueScore],dat: List[Judgement], markers: List[Marker], updated: List[Marker]):List[Marker] = markers match {
+    case Nil => updated
+    case _ =>
+    //filter judgements by marker 
+    val myJs = dat filter (x => x.marker==markers.head.id)
+    val probs: List[Double] = myJs.map(x => expon(ts.find{_.id == x.userchosen}.get.true_score,ts.find{_.id == x.usernotchosen}.get.true_score))
+    val numer: Double = probs.foldLeft(0.0d)((total,p)=>{
+      val res = 1 - p
+      val sq = math.pow(res,2)
+      val info = p * (1 - p)
+      val zsq = sq / info
+      val num = info * zsq
+      total + num 
+    })
+    val denom: Double = probs.foldLeft(0.0d)((total,item)=>{
+      val info = item * (1 - item)
+      total + info 
+    })
+    val wms = numer / denom
+    
+    markerInfit(ts, dat, markers.tail, new Marker(markers.head.id,wms) :: updated)
+    
+  }
                                                   
   def expon(va: Double, vi: Double):Double =
     math.exp(va - vi) / (1 + math.exp(va - vi))   
@@ -26,29 +57,29 @@ object ml {
     case Nil => updated
     case _ =>
       //println("Estimating person id: "+ids.head.id)
-      var myGames = dat filter (x => x.userchosen == ids.head.id || x.usernotchosen == ids.head.id)
-      var timesTaken = myGames map (x => x.timetaken)
-      var timetaken = timesTaken.sum
-      var opps = myGames map (x => if (x.userchosen==ids.head.id) x.usernotchosen else x.userchosen)
+      val myGames = dat filter (x => x.userchosen == ids.head.id || x.usernotchosen == ids.head.id)
+      val timesTaken = myGames map (x => x.timetaken)
+      val timetaken = timesTaken.sum
+      val opps = myGames map (x => if (x.userchosen==ids.head.id) x.usernotchosen else x.userchosen)
       //println("Opponents: "+opps)
       //println("all_ids: "+all_ids)
-      var opp_ts: List[Double] = opps map (x => all_ids.find{_.id == x}.get.true_score)
+      val opp_ts: List[Double] = opps map (x => all_ids.find{_.id == x}.get.true_score)
       //println("Opponents Ts: "+opp_ts)
       
-      var myScores = toys(ids.head.id, ids.head.true_score, myGames)
+      val myScores = toys(ids.head.id, ids.head.true_score, myGames)
       //println(myScores)
-      var obs_score = myScores.filter(xy => xy._2 == 1).length
+      val obs_score = myScores.filter(xy => xy._2 == 1).length
       //println("obs: " + obs_score)
       //println(ids.head.true_score + "vs ?")
       
       //Calculate expected score
-      var exp_scores = opp_ts map(x => expon(ids.head.true_score ,x))
-      var exp_score = exp_scores.sum
+      val exp_scores = opp_ts map(x => expon(ids.head.true_score ,x))
+      val exp_score = exp_scores.sum
       
       //println("exp:" + exp_score)
       //println("current ts estimate:" + ids.head.true_score)
-      var info = myScores.map(xy => (1 - expon(ids.head.true_score , xy._1))* (expon(ids.head.true_score , xy._1))).sum
-      var update = ids.head.true_score + ((obs_score - exp_score)/info)
+      val info = myScores.map(xy => (1 - expon(ids.head.true_score , xy._1))* (expon(ids.head.true_score , xy._1))).sum
+      val update = ids.head.true_score + ((obs_score - exp_score)/info)
       //println("updated:" + update)
       byId(all_ids, ids.tail, dat, new TrueScore(ids.head.id,update,1/math.sqrt(info),obs_score,myGames.length,timetaken)::updated)
   }                                               
@@ -100,6 +131,19 @@ def variance[T](items:Traversable[T])(implicit n:Numeric[T]) : Double = {
     total + square
   })
   sumOfSquares / count.toDouble
+}
+
+def raschRel(ts: List[TrueScore]): Double = {
+  val tse = ts.map(x => x.se)
+  val rms = tse.foldLeft(0.00d)((total, item)=> {
+    val square = math.pow(item, 2)
+    total + square
+  })
+  val rmse = math.pow((rms / ts.length),0.5)
+  val tss =ts.map(x => x.true_score)
+  val G = stddev(tss) / rmse
+  val alpha = (math.pow(G,2) - 1) / math.pow(G,2) 
+  alpha
 }
   
 def stddev[T](items:Traversable[T])(implicit n:Numeric[T]) : Double = {
